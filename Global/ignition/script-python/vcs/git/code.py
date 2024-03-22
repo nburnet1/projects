8ce44a755357d2f0da367a34a1fe6541e40d936f
nburnet1 <noah.burnette@intellicintegration.com>
2024-03-22 09:39:03 -0400

    Designer save @ 03-22-2024 09:39:03

---
+++ Global/ignition/script-python/vcs/git/code.py
@@ -0,0 +1,26 @@
+def commit():
+	import system
+	import time
+	
+	tagProviders= system.db.runPrepQuery("Select NAME from TAGPROVIDERSETTINGS WHERE NAME != 'default'",database="IgnitionInternal")
+	tagProviders = [tagProviders[row]['NAME'] for row in range(len(tagProviders))]
+	
+	for tp in tagProviders:
+		filePath = "/usr/local/bin/ignition/data/projects/.tags/"+tp+"/tags.json"
+		tags = system.tag.exportTags(filePath=filePath, tagPaths=["["+tp+"]"], recursive=True)
+	time.sleep(5)
+	system.util.execute(["/usr/local/bin/ignition/data/projects/.scripts/git-auto-commit.sh"])
+	
+system.util.invokeAsynchronous(commit)
+
+def importTags():
+	import os 
+	#system.util.execute(["/usr/local/bin/ignition/data/projects/.scripts/git-startup-restore.sh"])
+	
+	projectDir = "/usr/local/bin/ignition/data/projects/.tags"
+	
+	tagDirs = os.listdir(projectDir)
+	
+	for tagProvider in tagDirs:
+		filePath = projectDir + "/"+ tagProvider + "/tags.json"
+		system.tag.importTags(filePath,"["+tagProvider+"]", "o")
