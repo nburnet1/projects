64f71d04aa74d9fcb89985e520c255a92c3c36c5
nburnet1 <noah.burnette@intellicintegration.com>
2024-03-25 14:03:29 -0400

    changed git commit

--- .scripts/git-commit.sh
+++ .scripts/git-commit.sh
@@ -1,4 +1,4 @@
 cd /usr/local/bin/ignition/data/projects
 NOW=$(date +"%m-%d-%Y %H:%M:%S")
 commitMessage="$1" || commitMessage="Designer save @ $NOW"
-git commit -m "$commitMessage"
+git commit -m "$commitMessage" > git.log
