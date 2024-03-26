cce4be7eddab7bd6facbb77969fb3048ae962190
Noah Burnette <80188246+nburnet1@users.noreply.github.com>
2024-03-26 09:26:51 -0400

    Update git-status.sh

--- .scripts/git-status.sh
+++ .scripts/git-status.sh
@@ -1,4 +1,4 @@
 cd /usr/local/bin/ignition/data/projects/$1
 # Args:
 # project name
-git status . &> /usr/local/bin/ignition/data/projects/$1/git.log
+git status . > /usr/local/bin/ignition/data/projects/$1/git.log
